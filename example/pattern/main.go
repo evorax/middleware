e0abca9815a09215e1170405022d566a788f8af6
evorax <discorddev@skiff.com>
2024-06-12 08:08:08 +0900

    update example

--- example/pattern/main.go
+++ example/pattern/main.go
@@ -10,11 +10,11 @@ func main() {
 	e := middle.New()
 
 	e.GET(`/foo/:id[^[a-zA-Z]+$]/:id2[^\d+$]`, func(ctx *middle.Context) {
-		fmt.Fprintf(ctx.Writer, fmt.Sprintf("id:%v id2:%v", ctx.Params["id"], ctx.Params["id2"]))
+		ctx.Write(200, fmt.Sprintf("id:%s id2:%s", ctx.GetParam("id"), ctx.GetParam("id2")))
 	})
 
 	e.GET(`/abc/*/:id`, func(ctx *middle.Context) {
-		fmt.Fprintf(ctx.Writer, fmt.Sprintf("id:%v", ctx.Params["id"]))
+		ctx.Write(200, ctx.GetParam("id"))
 	})
 
 	e.Listen(":8080")
